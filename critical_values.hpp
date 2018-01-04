e0e9e8317ae0d1d7a4278c348f5628b6770a70d1
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-01-04 18:43:22 +0100

    Fix AD critical values
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

--- critical_values.hpp
+++ critical_values.hpp
@@ -1,7 +1,6 @@
 #include <cassert>
 #include <cmath>
 
-
 template <int alpha_num, int alpha_den>
 inline double get_ks_critical(unsigned long n) {
 	constexpr double alpha = ((double)alpha_num)/((double)alpha_den);
@@ -15,7 +14,7 @@ inline double get_ks_critical(unsigned long n) {
 
 
 template <int alpha_num, int alpha_den, bool upper_only=false>
-class AndersonDarlingCV_EV {
+class AndersonDarlingCV {
 
 	// From: "Approximation of modified Anderson-Darling test statisitcs for extreme value distributions with unknown shape parameter"
 	//		J. Heo, H. Shin, W. Nam, J. Om, C. Jeong
@@ -23,92 +22,111 @@ class AndersonDarlingCV_EV {
 };
 
 template <>
-class AndersonDarlingCV_EV<5, 100, true> {
-
+class AndersonDarlingCV<5, 100> {
 public:
-
-	static constexpr double safety_margin = 0.05;
-	
-	static double get_critical_value(unsigned long n, double shape_param) noexcept {
-
-		constexpr double a = 0.2776;
-		constexpr double b = -0.2441;
-		constexpr double c = 1.8927;
-		constexpr double d = -0.0267;
-		constexpr double e = 0.1586;
-		
-
-		double table_cv = a + b / n + c / (n*n) + d * (-shape_param) + e * shape_param * shape_param;
-
-		return table_cv + table_cv * safety_margin;
+	static double get_critical_value(unsigned int cardinality, double shape_param) {
+		switch(cardinality) {
+		case 50:
+			 if (shape_param==-0.5) return 2.47412;
+			 if (shape_param== 0.0) return 2.50976;
+			 if (shape_param== 0.5) return 2.53496;
+		break;
+		case 100:
+			 if (shape_param==-0.5) return 2.49509;
+			 if (shape_param== 0.0) return 2.52714;
+			 if (shape_param== 0.5) return 2.42694;
+		break;
+		case 250:
+			 if (shape_param==-0.5) return 2.48027;
+			 if (shape_param== 0.0) return 2.52288;
+			 if (shape_param== 0.5) return 2.50464;
+		break;
+		case 500:
+			 if (shape_param==-0.5) return 2.46681;
+			 if (shape_param== 0.0) return 2.44137;
+			 if (shape_param== 0.5) return 2.48736;
+		break;
+		case 750:
+			 if (shape_param==-0.5) return 2.50228;
+			 if (shape_param== 0.0) return 2.48115;
+			 if (shape_param== 0.5) return 2.50141;
+		break;
+		case 1000:
+			 if (shape_param==-0.5) return 2.44515;
+			 if (shape_param== 0.0) return 2.46962;
+			 if (shape_param== 0.5) return 2.52046;
+		break;
+		case 2500:
+			 if (shape_param==-0.5) return 2.56467;
+			 if (shape_param== 0.0) return 2.54922;
+			 if (shape_param== 0.5) return 2.44433;
+		break;
+		case 5000:
+			 if (shape_param==-0.5) return 2.47965;
+			 if (shape_param== 0.0) return 2.48939;
+			 if (shape_param== 0.5) return 2.47019;
+		break;
+		case 10000:
+			 if (shape_param==-0.5) return 2.51519;
+			 if (shape_param== 0.0) return 2.53523;
+			 if (shape_param== 0.5) return 2.5269;
+		break;
+		}
+		assert(false);
 	}
-
 };
-
 template <>
-class AndersonDarlingCV_EV<1, 100, true> {
-
+class AndersonDarlingCV<1, 100> {
 public:
-
-	static constexpr double safety_margin = 0.05;
-	
-	static double get_critical_value(unsigned long n, double shape_param) noexcept {
-
-		constexpr double a = 0.3828;
-		constexpr double b = -0.4179;
-		constexpr double c = 2.5682;
-		constexpr double d = -0.0362;
-		constexpr double e = 0.2134;
-		
-
-		double table_cv = a + b / n + c / (n*n) + d * (-shape_param) + e * shape_param * shape_param;
-
-		return table_cv + table_cv * safety_margin;
+	static double get_critical_value(unsigned int cardinality, double shape_param) {
+		switch(cardinality) {
+		case 50:
+			 if (shape_param==-0.5) return 3.77154;
+			 if (shape_param== 0.0) return 3.86773;
+			 if (shape_param== 0.5) return 3.74202;
+		break;
+		case 100:
+			 if (shape_param==-0.5) return 3.86384;
+			 if (shape_param== 0.0) return 3.87911;
+			 if (shape_param== 0.5) return 3.89836;
+		break;
+		case 250:
+			 if (shape_param==-0.5) return 3.81976;
+			 if (shape_param== 0.0) return 3.93195;
+			 if (shape_param== 0.5) return 3.93606;
+		break;
+		case 500:
+			 if (shape_param==-0.5) return 3.92627;
+			 if (shape_param== 0.0) return 3.86792;
+			 if (shape_param== 0.5) return 3.84262;
+		break;
+		case 750:
+			 if (shape_param==-0.5) return 3.90418;
+			 if (shape_param== 0.0) return 3.68703;
+			 if (shape_param== 0.5) return 3.86732;
+		break;
+		case 1000:
+			 if (shape_param==-0.5) return 3.91767;
+			 if (shape_param== 0.0) return 3.88882;
+			 if (shape_param== 0.5) return 3.8788;
+		break;
+		case 2500:
+			 if (shape_param==-0.5) return 4.06825;
+			 if (shape_param== 0.0) return 3.98086;
+			 if (shape_param== 0.5) return 3.92626;
+		break;
+		case 5000:
+			 if (shape_param==-0.5) return 3.8144;
+			 if (shape_param== 0.0) return 3.88781;
+			 if (shape_param== 0.5) return 3.89555;
+		break;
+		case 10000:
+			 if (shape_param==-0.5) return 3.74082;
+			 if (shape_param== 0.0) return 3.80453;
+			 if (shape_param== 0.5) return 3.88444;
+		break;
+		}
+		assert(false);
 	}
-
 };
 
-template <>
-class AndersonDarlingCV_EV<5, 100> {
-
-public:
-
-	static constexpr double safety_margin = 0.05;
-	
-	static double get_critical_value(unsigned long n, double shape_param) noexcept {
-
-		constexpr double a = 0.5507;
-		constexpr double b = 0.7928;
-		constexpr double c = -3.5521;
-		constexpr double d = -0.2103;
-		constexpr double e = 0.3388;
-		
-
-		double table_cv = a + b / n + c / (n*n) + d * (-shape_param) + e * shape_param * shape_param;
-
-		return table_cv + table_cv * safety_margin;
-	}
-
-};
-
-template <>
-class AndersonDarlingCV_EV<1, 100> {
-
-public:
-	static constexpr double safety_margin = 0.05;
-	
-	static double get_critical_value(unsigned long n, double shape_param) noexcept {
-
-		constexpr double a = 0.7385;
-		constexpr double b = 2.5585;
-		constexpr double c = -14.2247;
-		constexpr double d = -0.5708;
-		constexpr double e = 0.8825;
-		
-
-		double table_cv = a + b / n + c / (n*n) + d * (-shape_param) + e * shape_param * shape_param;
-
-		return table_cv + table_cv * safety_margin;
-	}
-
-};
--- main.cpp
+++ main.cpp
@@ -9,66 +9,14 @@
 #include <iostream>
 #include <omp.h>
 
-#define DIST_T_STUDENT 1
-#define DIST_UNIFORM 2
-#define DIST_EVT0 3
-#define DIST_EVT_0_5 4
-#define DIST_EVT_N_0_5 5
-#define DIST_NORMAL 6
-
-//#define H1 DIST_EVT_0_5
-
-//#define H0 DIST_EVT_0_5
-
-//#define DEBUG 1
-
-#define TEST_KS 1
-#define TEST_AD 2
-
-#if H0 == DIST_EVT0
-	#define EVT_PARAM_NUM 0
-	#define EVT_PARAM_DEN 1
-constexpr double evt_param = 0.0;
-#elif H0 == DIST_EVT_0_5
-	#define EVT_PARAM_NUM 1
-	#define EVT_PARAM_DEN 2
-constexpr double evt_param = 0.5;
-#elif H0 == DIST_EVT_N_0_5
-	#define EVT_PARAM_NUM -1
-	#define EVT_PARAM_DEN 2
-constexpr double evt_param = -0.5;
-#else
-	#error "Unknown distribution"
-#endif
-
-#if H1 == DIST_T_STUDENT
-	#define MONTE_CARLO_GENERATOR montecarlo_frequencies<T_STUDENT>
-	#define MONTE_CARLO_SAMPLING montecarlo_t_student_sample
-#elif H1 == DIST_UNIFORM
-	#define MONTE_CARLO_GENERATOR montecarlo_frequencies<UNIFORM>
-	#define MONTE_CARLO_SAMPLING montecarlo_uniform_sample
-#elif H1 == DIST_NORMAL
-	#define MONTE_CARLO_GENERATOR montecarlo_frequencies<NORMAL>
-	#define MONTE_CARLO_SAMPLING montecarlo_normal_sample
-#elif H1 == DIST_EVT0
-	#define MONTE_CARLO_GENERATOR montecarlo_frequencies_evt<0,1>
-	#define MONTE_CARLO_SAMPLING montecarlo_evt_sample<0, 1>
-#elif H1 == DIST_EVT_0_5
-	#define MONTE_CARLO_GENERATOR montecarlo_frequencies_evt<1,2>
-	#define MONTE_CARLO_SAMPLING montecarlo_evt_sample<1, 2>
-#elif H1 == DIST_EVT_N_0_5
-	#define MONTE_CARLO_GENERATOR montecarlo_frequencies_evt<-1,2>
-	#define MONTE_CARLO_SAMPLING montecarlo_evt_sample<-1, 2>
-#else
-	#error "Unknown"
-#endif
-
 unsigned int config::sample_cardinality;
 
 template<int alpha_num, int alpha_den>
 unsigned long perform_ad_run() {
 
-	double ad_critical_value = AndersonDarlingCV_EV<alpha_num, alpha_den, false>::get_critical_value(config::sample_cardinality, evt_param);
+	constexpr double safe_margin = 0.05;
+
+	double ad_critical_value = (1. + safe_margin) * AndersonDarlingCV<alpha_num, alpha_den, false>::get_critical_value(config::sample_cardinality, evt_param);
 
 	unsigned long reject=0;
 

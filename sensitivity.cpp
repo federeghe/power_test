002a9c19757d28db4f19dfdffa272212206d4c20
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-01-25 23:30:42 +0100

    Add sensitivity for MAD
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

--- sensitivity.cpp
+++ sensitivity.cpp
@@ -1,19 +1,112 @@
+#include "test_statistics.hpp"
 #include "critical_values.hpp"
 #include "montecarlo.hpp"
 #include "distributions.hpp"
 #include "simulation.hpp"
 
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
 #include <omp.h>
 
 #define NR_RUNS 1000
+#define NR_RUNS_CRIT 10000
 #define DEBUG 0
 
+#define TEST_KS  1
+#define TEST_AD  2 
+#define TEST_MAD 3
 
 unsigned int config::sample_cardinality;
 
+template <int num, int den, int significance_num, int significance_den>
+double compute_ad_crit_value() {
+	std::vector<double> crits;
+	crits.resize(NR_RUNS_CRIT);
 
+	#pragma omp parallel 
+	{
+		std::vector<double> sample;
+		sample.resize(config::sample_cardinality);
+
+		std::mt19937 random_gen(std::random_device{}());
+		#pragma omp for
+		for (unsigned long i=0; i < NR_RUNS_CRIT; i++) {
+			for (unsigned int j=0; j < config::sample_cardinality; j++) {
+				sample[j] = montecarlo_evt_sample<num, den>(random_gen);
+			}
+			std::sort(sample.begin(), sample.end());
+#if TEST==TEST_AD
+			crits[i] = get_ad_statistic<num, den>(sample);
+#endif
+#if TEST==TEST_MAD
+			crits[i] = get_ad_statistic_upper<num, den>(sample);
+#endif
+		}
+	}
+	std::sort(crits.begin(),crits.end());
+
+	return crits[(int)(((double)significance_num/(double)significance_den) * NR_RUNS_CRIT)];
+}
+
+#ifndef TEST
+	#error TEST must be defined
+#endif
+
+#if TEST==TEST_AD || TEST==TEST_MAD
+template <int num, int den>
+static inline void run() noexcept {
+	constexpr double xi = ((double)num) / ((double)den);
+
+	const double ad_critical_value1 = compute_ad_crit_value<0, 1, 99, 100>();
+	const double ad_critical_value2 = compute_ad_crit_value<0, 1, 95, 100>();
+
+	std::cout << config::sample_cardinality << " " << xi << " ";
+
+	unsigned long reject1=0;
+	unsigned long reject2=0;
+
+	#pragma omp parallel
+	{
+		thread_local static std::mt19937 random_gen(std::random_device{}());
+		std::vector<double> sample;
+		sample.resize(config::sample_cardinality);
+
+		#pragma omp for reduction(+:reject1,reject2)
+		for (unsigned long j=0; j < NR_RUNS; j++) {
+
+
+			for (unsigned int i=0; i < config::sample_cardinality; i++) {
+				sample[i] = montecarlo_evt_sample<num, den>(random_gen);
+			}
+
+			std::sort(sample.begin(), sample.end());
+
+#if TEST == TEST_AD
+			double S = get_ad_statistic<0, 1>(sample);
+#elif TEST == TEST_MAD
+			double S = get_ad_statistic_upper<0, 1>(sample);
+#endif
+
+			if ( S > ad_critical_value1 ) {
+				reject1++;
+			}
+			if ( S > ad_critical_value2 ) {
+				reject2++;
+			}
+
+		}
+	
+	}
+	std::cout << reject1;
+	std::cout << " ";
+	std::cout << reject2;
+	std::cout << std::endl;
+
+}
+#endif
+
+#if TEST==1
 template <int num, int den>
 static inline void run() noexcept {
 	constexpr double xi = ((double)num) / ((double)den);
@@ -87,7 +180,7 @@ static inline void run() noexcept {
 	std::cout << std::endl;
 
 }
-
+#endif
 
 template<int x, int to>
 struct static_for
@@ -111,7 +204,7 @@ int main(int argc, char* argv[]) {
 
 
 	std::vector<unsigned int> cardinalities;
-	for (int i=0; i<1000; i++) {
+	for (int i=0; i<500; i++) {
 		cardinalities.push_back((i+1)*10);
 	}
 
